502be4a4cbcfaf6b08863cb88fb11094028761e8
kocakaysegul <aysegulkocak112@gmail.com>
2023-01-18 10:37:30 +0300

    simple login-register exp created by using firebase auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
@@ -13,15 +15,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -30,16 +23,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  const MyHomePage({super.key, required this.title});
 
   final String title;
 
@@ -48,68 +32,162 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  var _isLogin = false;
+  final cont_login_user = TextEditingController();
+  final cont_login_pass = TextEditingController();
+  var _message = "";
+  var _token = null;
+  var _expireDate = null;
+  var _userId = null;
 
-  void _incrementCounter() {
+  Future<void> signUpFunction() async {
+    var url = Uri.parse(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCiH0caU4L8FWb_tllR_WCWgBtdS9BDMFQ');
+    try {
+      final response = await http.post(url,
+          body: json.encode({
+            'email': cont_login_user.text,
+            'password': cont_login_pass.text,
+            'returnSecureToken': true
+          }));
+      final returnData = json.decode(response.body);
+      if (returnData['error'] != null) {
+        setState(() {
+          _message = returnData['error']['message'];
+        });
+      }
+      if (returnData['kind'] != null) {
+        setState(() {
+          _message = returnData['kind'];
+        });
+      }
+      print(returnData);
+    } catch (e)
+    {
+      print(e.toString());
+    }
+  }
+
+  Future<void> signInFunction() async {
+    var url = Uri.parse(
+
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCiH0caU4L8FWb_tllR_WCWgBtdS9BDMFQ');
+    try {
+      final response = await http.post(url,
+          body: json.encode({
+            'email': cont_login_user.text,
+            'password': cont_login_pass.text,
+            'returnSecureToken': true
+          }));
+      final returnData = json.decode(response.body);
+      if (returnData['error'] != null) {
+        setState(() {
+          _message = returnData['error']['message'];
+        });
+      }
+      if (returnData['kind'] != null) {
+        setState(() {
+          _message = returnData['kind'];
+          _isLogin = true;
+          _userId = returnData['localId'];
+          _token = returnData['idToken'];
+          _expireDate = DateTime.now().add(Duration(seconds:
+          int.parse(returnData['expiresIn'])));
+        });
+      }
+      print(_expireDate.toString());
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  void signOutFunction() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _isLogin = false;
+      _token = null;
+      _expireDate = null;
     });
+  }
+
+  Future<void> getDataFunction() async {
+    var url = Uri.parse(
+        'https://flutter-demo-b5b5c-default-rtdb.europewest1.firebasedatabase.app/test1.json?auth=$_token');
+    try {
+      var response = await http.get(url);
+      //print(response.body);
+      var data = json.decode(response.body);
+      print(data);
+      data.forEach((k, v) {
+        print('${k}');
+        var data2 = v;
+        data2.forEach((k2, v2) {
+          print('${k2}:${v2}');
+        });
+      });
+    } catch (error) {
+      throw (error);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            _isLogin
+                ? Column(
+              children: [
+                Text(
+                  'You have logged in.',
+                ),
+                TextButton(onPressed: () {
+                  getDataFunction();
+                }, child: const Text('Get Data')),
+                TextButton(onPressed: () {
+                  signOutFunction();
+                }, child: const Text('Logout'))
+              ],
+            )
+                : Column(
+              children: [
+                Text(
+                  'No one logged in. $_message',
+                ),
+                TextField(
+                  controller: cont_login_user,
+                  decoration: InputDecoration(border: OutlineInputBorder(),
+                    labelText: 'Email',
+                  ),
+                ),
+                TextField(
+                  controller: cont_login_pass,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Password',
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    signUpFunction();
+                  },
+                  child: const Text('Register'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    signInFunction();
+                  },
+                  child: const Text('Login'),
+                )
+              ],
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
